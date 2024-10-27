a03e3238ac830d72d183f678bbc358f3621bfb5c
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2024-10-27 15:52:17 +0100

    Update main.lua
    1. **Improved Logic for Sleep Periods**:
    In each `while` loop, a shorter `Wait` time is only used if the player is armed or performing a specific action. Otherwise, the `Wait` timer is set to `1000` milliseconds to reduce CPU load.
    
    2. **Better Structuring and Clarity**:
    Loops and conditions have been optimized for readability and maintainability, making the code easier to understand and manage.
    
    3. **Performance and Maintainability**:
     The check `IsPedArmed(PlayerPedId(), 6)` is used consistently to only execute actions when the player is actually armed, which improves performance by reducing unnecessary checks.

--- client/main.lua
+++ client/main.lua
@@ -1,145 +1,91 @@
 local ActiveRecoil = false
 
 Citizen.CreateThread(function()
-
     if Config.CustomDamage then
-        for k, v in pairs(Config.Weapons) do
-
-            SetWeaponDamageModifier(k, v.Damage)
-    
+        for weapon, data in pairs(Config.Weapons) do
+            SetWeaponDamageModifier(weapon, data.Damage)
         end
     end
-    
+
     if Config.ClassicRecoil then
-        for k,v in pairs(Config.Weapons) do
-
-            SetWeaponRecoilShakeAmplitude(GetHashKey(k), Config.Weapons[k].ClassicRecoil)
-
+        for weapon, data in pairs(Config.Weapons) do
+            SetWeaponRecoilShakeAmplitude(GetHashKey(weapon), data.ClassicRecoil)
         end
     end
 
     if Config.RealisticFlashLight then
-
         SetFlashLightKeepOnWhileMoving(true)
-
     end
-
 end)
 
-
 Citizen.CreateThread(function()
-
-    while (Config.HideCrossHair) do
-
+    while Config.HideCrossHair do
         Citizen.Wait(1)
-
-        local sleep = true
+        local isArmed = IsPedArmed(PlayerPedId(), 6)
         
-        if IsPedArmed(PlayerPedId(), 6) then
-            sleep = false
+        if isArmed then
             HideHudComponentThisFrame(14)
-        end
-
-        if sleep then
-            Wait(1000)
+        else
+            Citizen.Wait(1000)
         end
     end
-
-
 end)
 
 Citizen.CreateThread(function()
-
-    while (Config.DisableHeadShots) do
-
+    while Config.DisableHeadShots do
         Citizen.Wait(5000)
-
         SetPedSuffersCriticalHits(PlayerPedId(), false)
-
     end
-
 end)
 
 Citizen.CreateThread(function()
-
-    while (Config.RemovePistolWhipping) do
-
+    while Config.RemovePistolWhipping do
         Citizen.Wait(15)
-        local sleep = true
-
         if IsPedArmed(PlayerPedId(), 6) then
-            sleep = false
-
             DisableControlAction(1, 140, true)
             DisableControlAction(1, 141, true)
             DisableControlAction(1, 142, true)
-
+        else
+            Citizen.Wait(1000)
         end
-
-        if sleep then
-            Wait(1000)
-        end
-
     end
-    
-end) 
-
-Citizen.CreateThread(function()
-
-    while (Config.DrunkRecoil) do
-
-        Citizen.Wait(150)
-        local sleep = true
-
-        if IsPedArmed(PlayerPedId(), 6) then
-            sleep = false
-
-            local CurrentWeapon = nil
-            local _, Weapon = GetCurrentPedWeapon(PlayerPedId())
-
-            for k in pairs(Config.Weapons) do
-                local key = GetHashKey(k)
-
-                if key == Weapon then
-                    CurrentWeapon = k
-                end
-
-            end
-                
-            if CurrentWeapon ~= nil then
-
-                local Player = PlayerId()
-                local PlayerPed = PlayerPedId() 
-
-                if IsPlayerFreeAiming(Player) or IsPedShooting(PlayerPed) and Config.Weapons[CurrentWeapon].DrunkRecoil ~= 0 then
-
-                    if not ActiveRecoil then
-                        ActiveRecoil = true
-                        ShakeGameplayCam("DRUNK_SHAKE", Config.Weapons[CurrentWeapon].DrunkRecoil)
-                    end
-                else
-
-                    if ActiveRecoil then
-
-                        ActiveRecoil = false
-                        Wait(150)
-                        ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-    
-                    end
-
-                end
-            else
-                sleep = true
-            end
-        end
-
-        if sleep then
-            Wait(1000)
-        end
-
-    end
-
 end)
 
+Citizen.CreateThread(function()
+    while Config.DrunkRecoil do
+        Citizen.Wait(150)
 
+        local isArmed = IsPedArmed(PlayerPedId(), 6)
+        if isArmed then
+            local currentWeapon = nil
+            local _, weapon = GetCurrentPedWeapon(PlayerPedId())
 
+            for weaponName, data in pairs(Config.Weapons) do
+                if GetHashKey(weaponName) == weapon then
+                    currentWeapon = weaponName
+                    break
+                end
+            end
+            
+            if currentWeapon and Config.Weapons[currentWeapon].DrunkRecoil ~= 0 then
+                local playerPed = PlayerPedId()
+                local isAimingOrShooting = IsPlayerFreeAiming(PlayerId()) or IsPedShooting(playerPed)
+
+                if isAimingOrShooting then
+                    if not ActiveRecoil then
+                        ActiveRecoil = true
+                        ShakeGameplayCam("DRUNK_SHAKE", Config.Weapons[currentWeapon].DrunkRecoil)
+                    end
+                elseif ActiveRecoil then
+                    ActiveRecoil = false
+                    Citizen.Wait(150)
+                    ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+                end
+            else
+                Citizen.Wait(1000)
+            end
+        else
+            Citizen.Wait(1000)
+        end
+    end
+end)
